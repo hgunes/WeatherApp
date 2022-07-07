bad9c714778b49c7f192688dfe9fb20807f1728d
harungunes <h.gunes13@gmail.com>
2022-07-07 21:38:39 +0200

    init repository

---
+++ WeatherApp/TabbarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabbarController.swift
+//  WeatherApp
+//
+//  Created by Harun Gunes on 07/07/2022.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBlue
+    
+  }
+  
+  
+}
+
