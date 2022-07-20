31c79fa924664477d175a53675e73a30eb7a767e
hgunes <h.gunes13@gmail.com>
2022-07-20 21:34:19 +0200

    test: add tests to city view model

---
+++ WeatherAppTests/CityViewModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  CityViewModelTests.swift
+//  WeatherAppTests
+//
+//  Created by Harun Gunes on 20/07/2022.
+//
+
+import XCTest
+@testable import WeatherApp
+
+class CityViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    
+    func testCityViewModel() throws {
+        
+        let weatherData = WeatherData(weather: [Weather(id: 1, main: "test main")], main: Main(temp: 29.37, feelsLike: 26.23, humidity: 47), name: "Erzurum")
+
+        
+        let cityViewModel = CityViewModel(weatherData: weatherData)
+        
+        XCTAssertEqual(weatherData.name, cityViewModel.city)
+        XCTAssertEqual(weatherData.main.temp, cityViewModel.temperature)
+    }
+}
